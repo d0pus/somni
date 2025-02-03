e9ca15e9da94e45ccd2edc2d1cd9207c53703a59
D0pus <mopsuspro@gmail.com>
2025-02-03 19:58:36 +0300

    Фикс скролла, луны, графика и свечения

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,7 +3,7 @@ import SleepChart from './SleepChart';
 
 const Hero = () => {
   return (
-    <div className="relative flex flex-col md:flex-row items-center justify-start min-h-screen px-4 md:px-8 max-w-7xl mx-auto">
+    <div className="relative flex flex-col items-center justify-start min-h-screen px-4 md:px-8 overflow-auto">
       
 
       <div className="w-full md:w-1/2 text-center md:text-left mt-10 md:mt-0">
@@ -22,13 +22,13 @@ const Hero = () => {
       <div className="w-full md:w-1/2 flex flex-col items-center mt-12 md:mt-0">
         
 
-        <div className="relative w-full flex justify-center md:justify-end">
+        <div className="absolute top-0 left-1/2 -translate-x-1/2 mt-4 sm:mt-10">
           <Moon size={80} className="text-moonYellow w-[80px] h-[80px] md:w-[120px] md:h-[120px] animate-float" />
-          <div className="absolute inset-0 blur-xl bg-moonYellow/20 rounded-full" />
+          <div className="absolute w-[100px] h-[100px] md:w-[150px] md:h-[150px] blur-lg bg-moonYellow/30 rounded-full" />
         </div>
 
 
-        <div className="w-full mt-12 px-4 sm:px-8">
+        <div className="w-full mt-40 sm:mt-64 px-4 sm:px-8">
           <SleepChart />
         </div>
 
